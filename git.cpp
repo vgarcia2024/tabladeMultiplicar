87ecfcfee6ecd6920829a0b3b0f6f6a5aa0879f1
vgarcia <vgarcia@frsn.utn.edu.ar>
2024-09-10 15:20:00 -0300

    Primer commit, tabla de multiplicar

---
+++ git.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero;
+
+    cout << "Ingrese un numero: ";
+    cin >> numero;
+
+    for(int i = 1; i <= 10; i++){
+        cout << "La tabla de multiplicar del " << numero << " es " << " = " << numero * i << endl;
+    }
+
+
+
+
+
+
+
+    return 0;
+}
